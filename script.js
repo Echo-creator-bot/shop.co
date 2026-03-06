a69139d5b568f93d2b1c79a425073b384bddda79
Skemley Devar <a89042027062@gmail.com>
2026-03-06 23:38:09 +0300

    first page button animation and initial logic

--- script.js
+++ script.js
@@ -1,3 +1,74 @@
+/* фокус на инпут при клике по иконке */
+const Search_Icon = document.querySelector('.search-svg-wrap');
+const Search_input = document.querySelector('.header-top-search-input');
+Search_Icon.addEventListener('click', function(){
+  Search_input.focus();
+});
+/* ----------------------------------------------------------*/
+/*const burger = document.getElementById('burger');
+const menu = document.querySelector('.header-top-nav')
+burger.addEventListener('click', function(){
+  menu.style.display = 'block';
+});
+document.addEventListener('click', function(){
+  menu.style.display = 'none';
+});
+
+*/
+/*--------------------------------------------------------- */
+/* при клике на на список выпадает меню */
+/*const Drop_Down = document.querySelector('.dropdown');
+const Dropdown_Menu = document.querySelector('.dropdown-menu');
+let is_Open = false;
+function Open_dropdown_menu (){
+  Dropdown_Menu.style.display = 'relative'
+  is_Open = true;
+}
+function Close_dropdown_menu (){
+  Dropdown_Menu.style.display = 'none'
+  is_Open = false;
+}
+function Toggle_Dropdown () {
+  if (is_Open) {
+    Close_dropdown_menu();
+  } else {
+    Open_dropdown_menu();
+  }
+}
+Drop_Down.addEventListener('click', function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  Toggle_Dropdown();
+});
+document.addEventListener('click', function(e){
+  if (!Drop_Down.contains(e.target) && !Dropdown_Menu.contains(e.target)) {
+    Close_dropdown_menu();
+  }
+});*/
+/*-----------------------------------------------------------------------------*/ 
+/**/
+const Shop_Now_button = document.querySelector('.ShopNow-link');
+const Decoration_Star_1 = document.querySelector('.star-settings-1');
+const Star_Path_1 = Decoration_Star_1.querySelector('path');
+const Decoration_Star_2 = document.querySelector('.star-settings-2');
+const Star_Path_2 = Decoration_Star_2.querySelector('path');
+
+Shop_Now_button.addEventListener('mouseenter', function() {
+  Star_Path_1.style.opacity = '0.6';
+  Star_Path_2.style.opacity = '0.6';
+  Decoration_Star_1.style.transform = 'translateY(-25px)';
+  Decoration_Star_2.style.transform = 'translateY(-15px)';
+})
+Shop_Now_button.addEventListener('click', function() {
+
+})
+Shop_Now_button.addEventListener('mouseleave', function() {
+  Decoration_Star_1.style.transform = 'translateY(15px)';
+  Decoration_Star_2.style.transform = 'translateY(5px)';
+  Star_Path_1.style.opacity = '1';
+  Star_Path_2.style.opacity = '1';
+})
+
 // Функция для включения/выключения режима слайдера
 function setupSlider(container) {
   // Проверяем, нужно ли включать слайдер
@@ -63,3 +134,10 @@ document.addEventListener('DOMContentLoaded', function() {
     setupSlider(row);
   });
 });
+/*-----------------------------------------------------------------------*/ 
+const Email_Input = document.querySelector('.email-for-form');
+const Email_Icon = document.querySelector('.wrap-icon-for-form-email');
+Email_Icon.addEventListener('click', function() {
+  Email_Input.focus();
+});
+
